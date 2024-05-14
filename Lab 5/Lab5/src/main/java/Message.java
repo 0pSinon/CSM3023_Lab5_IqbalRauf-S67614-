5f21972c3e4d0fe4fa2132cee0d129c290586c01
0pSinon <155339930+0pSinon@users.noreply.github.com>
2024-05-15 00:43:40 +0800

    Add files via upload

---
+++ Lab 5/Lab5/src/main/java/Message.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Iqbal Rauf
+ */
+public class Message {
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
